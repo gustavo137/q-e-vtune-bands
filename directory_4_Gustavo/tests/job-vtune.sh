5e92d7fcfa44d98b7f0ba23dc4501582aca8bf1c
gustavo137 <gparedes@ictp.it>
2025-07-08 13:45:12 +0200

    directory_4_Gustavo added

---
+++ directory_4_Gustavo/tests/job-vtune.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=48
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:15:00
+#SBATCH --error test.err                 # std-error file
+#SBATCH --output test.out                # std-output file
+#SBATCH --account=ICT24_MHPC_1           #Sis25_baroni    # account number
+#SBATCH --partition=g100_usr_prod
+#SBATCH --dependency=afterany:16530568
+
+## Modules
+module load profile/base
+module load intel/
+module load intel-oneapi-mpi/
+module load intel-oneapi-mkl/
+module load vtune 
+
+## # Routes to your newly compiled executables
+##export PW=/g100_work/Sis25_baroni/pietrod/build_intel_741/bin/pw.x 
+##export D3H=/g100_work/Sis25_baroni/pietrod/build_intel_741/bin/d3hess.x
+##export PH=/g100_work/Sis25_baroni/pietrod/build_intel_741/bin/ph.x
+
+export PW=/g100_work/ICT24_MHPC_1/gparedes/q-e/build/bin/pw.x
+export D3H=/g100_work/ICT24_MHPC_1/gparedes/q-e/build/bin/d3hess.x 
+export PH=/g100_work/ICT24_MHPC_1/gparedes/q-e/build/bin/ph.x
+export suffix=nn${SLURM_NNODES}_${SLURM_JOB_ID}
+
+## Control of threads 
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
+## # Execution of commands
+## scf.in created based on EPW/examples/sic/epw/scf.in
+## d3hess.in comming from 
+srun --cpu_bind=cores $PW -i scf.in  >  scf.out_${suffix}
+srun --cpu_bind=cores $D3H -i d3hess.in    >  d3hess.out_${suffix}
+#srun --cpu_bind=cores $PH       -i ph_irr32.in   >  ph_irr32.out_${suffix}
+#srun --cpu_bind=cores $PH   -nb 2     -i ph_irr32.in > ph_irr32_NB2.out_${suffix}
+
+### Running with profiling
+srun --cpu_bind=cores vtune -trace-mpi -collect hotspot -result-dir results_hotspot_${SLURM_JOB_ID} -- $PH -nb 2 -i ph_irr32_niter_4.in   >  ph_irr32_NB2_prof${suffix}
