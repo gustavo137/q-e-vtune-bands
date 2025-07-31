298b99e03a4cc5935863f71e79bb6a9213bbc2b0
gustavo137 <gparedes@ictp.it>
2025-07-31 10:35:14 +0200

    my work

---
+++ my_work/tests/job-leo-vtune.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:15:00
+#SBATCH --error=out/test.err                 # std-error file
+#SBATCH --output=out/test.out                # std-output file
+#SBATCH --account=ICT25_MHPC_0           #Sis25_baroni    # account number
+#SBATCH --partition=boost_usr_prod     # If you need to run debug, max 30? mins 2? nodes
+#SBATCH --qos=boost_qos_dbg
+##SBATCH --dependency=afterany:16530568
+
+## Modules
+module purge
+module load profile/base
+module load intel-oneapi-mpi/
+module load intel-oneapi-mkl/
+#module load cmake/3.27.9  # >=3.2
+module load intel-oneapi-compilers/2023.2.1
+
+#module load vtune 
+module load intel-oneapi-vtune/
+
+
+## # Routes to your newly compiled executables
+##export PW=/g100_work/Sis25_baroni/pietrod/build_intel_741/bin/pw.x 
+##export D3H=/g100_work/Sis25_baroni/pietrod/build_intel_741/bin/d3hess.x
+##export PH=/g100_work/Sis25_baroni/pietrod/build_intel_741/bin/ph.x
+export PW=/leonardo/home/userexternal/gparedes/qe-vtune-bands/build/bin/pw.x
+export D3H=/leonardo/home/userexternal/gparedes/qe-vtune-bands/build/bin/d3hess.x
+export PH=/leonardo/home/userexternal/gparedes/qe-vtune-bands/build/bin/ph.x
+export suffix=nn${SLURM_NNODES}_${SLURM_JOB_ID}
+
+## Control of threads 
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
+## # Execution of commands
+## scf.in created based on EPW/examples/sic/epw/scf.in
+## d3hess.in comming from
+
+### To calculate Charge density and .save folder with al the electric information
+srun --cpu_bind=cores $PW -i scf.in > out/scf.out_${suffix}
+
+###=============================== VTune =======================================
+### Read .save from SCF, calculates the dinamic matrix and desplazaments (matrix dyn)
+#srun --cpu_bind=cores $PH -i ph_irr32.in > out/ph_irr32.out_${suffix}
+#srun --cpu_bind=cores $PH -nb 2 -i ph_irr32.in > out/ph_irr32_NB2.out_${suffix}
+
+## Running with profiling
+srun --cpu_bind=cores vtune -trace-mpi -collect hotspot -result-dir out/results_hotspot_${SLURM_JOB_ID} -- $PH -nb 2 -i ph_irr32_niter_4.in > out/ph_irr32_NB2_prof${suffix}
+
+###=============================== end VTune ====================================
+
+### Use PH results (matrix .dyn) and the SCF info to calculates the 3 order derivatives 
+#srun --cpu_bind=cores $D3H -i d3hess.in > out/d3hess.out_${suffix}
+
